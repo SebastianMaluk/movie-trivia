6b616b49f49125bf2e62efbd6c653c2b514292d5
javiersoto99] <jisoto2@miuandes.cl>
2023-04-12 21:43:19 -0400

    agregar pregunta

--- Scripts/partida.js
+++ Scripts/partida.js
@@ -1,3 +1,15 @@
+//Modal
+const blurFondo = document.getElementById("blur");
+const preguntaModal = document.getElementById("preguntaModal");
+const cerrarModalBtn = document.getElementById("cerrarModalBtn");
+const abrirModalBtn = document.getElementById("abrirModalBtn");
+const textoPregunta = document.getElementById("textoPregunta");
+
+const respuestaPreguntonInput = document.getElementById(
+  "respuestaPreguntonInput"
+);
+const enviarPreguntaBtn = document.getElementById("enviarPreguntaBtn");
+
 window.onload = function onInitialized() {
   addParticipantes([
     "Nombre de Usuario 1",
@@ -9,6 +21,9 @@ window.onload = function onInitialized() {
     "Nombre de Usuario 7",
     "Nombre de Usuario 8",
   ]);
+
+  textoPregunta.innerHTML =
+    "Tú eres el Preguntón, ingresa tu pregunta para que inicie la ronda";
 };
 
 function addParticipantes(participantes) {
@@ -23,3 +38,22 @@ function addParticipantes(participantes) {
     container.appendChild(div);
   });
 }
+
+abrirModalBtn.onclick = function () {
+  preguntaModal.classList.remove("hidden");
+  blurFondo.classList.remove("hidden");
+};
+
+cerrarModalBtn.onclick = function () {
+  preguntaModal.classList.add("hidden");
+  blurFondo.classList.add("hidden");
+};
+
+enviarPreguntaBtn.onclick = function (event) {
+  event.preventDefault();
+  var pregunta = respuestaPreguntonInput.value;
+  textoPregunta.innerHTML = pregunta;
+  abrirModalBtn.classList.add("hidden");
+  preguntaModal.classList.add("hidden");
+  blurFondo.classList.add("hidden");
+};
