28b939c90f76b6dcb156d03b7fce2d30f712c412
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-04-24 16:37:16 -0400

    change XMLHttpRequest to fetch

--- Scripts/login.js
+++ Scripts/login.js
@@ -3,37 +3,31 @@ var password = document.getElementById("passwordInput");
 var loginBtn = document.getElementById("loginBtn");
 
 window.onload = function onInitialized() {
-  var httpRequest = new XMLHttpRequest();
-
   loginBtn.onclick = function () {
     var jsonData = JSON.stringify({
       username: "G18_" + username.value,
       password: password.value,
     });
 
-    httpRequest.open(
-      "POST",
-      "https://trivia-bck.herokuapp.com/api/token/",
-      true
-    );
-    httpRequest.setRequestHeader("Content-Type", "application/json");
-    httpRequest.send(jsonData);
-  };
-
-  httpRequest.onreadystatechange = () => {
-    try {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        console.log(httpRequest.status);
-        console.log(httpRequest.statusText);
-        if (httpRequest.status === 200) {
-          let loginJWTTokens = JSON.parse(httpRequest.responseText);
-          localStorage.setItem("refreshToken", loginJWTTokens["refresh"]);
-          localStorage.setItem("accessToken", loginJWTTokens["access"]);
-          window.location = "home.html";
+    fetch("https://trivia-bck.herokuapp.com/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: jsonData,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
         }
-      }
-    } catch (e) {
-      console.log(`Caught Exception: ${e.description}`);
-    }
+      })
+      .then((loginJWTTokens) => {
+        localStorage.setItem("refreshToken", loginJWTTokens["refresh"]);
+        localStorage.setItem("accessToken", loginJWTTokens["access"]);
+        window.location = "home.html";
+      })
+      .catch((error) => {
+        console.log(`Caught Exception: ${error}`);
+      });
   };
 };
