e668655e2bc079ee8db1eb0037f3ed5d635d055a
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-04-24 02:31:41 -0400

    GET partidas

--- Scripts/home.js
+++ Scripts/home.js
@@ -9,78 +9,103 @@ window.onload = async function onInitialized() {
   console.log(localStorage["accessToken"]);
 
   getPartidas();
-
-  var unirmeBtn = document.getElementById("unirmeBtn");
-  unirmeBtn.onclick = function () {
-    location.href = "lobby.html";
-  };
 };
 
 function getPartidas() {
-  // Para la api
+  // TODO: get auth
+  fetch("https://trivia-bck.herokuapp.com/api/games/", {
+    headers: {
+      authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgyMzE4MTU3LCJpYXQiOjE2ODIzMTc4NTcsImp0aSI6ImUxY2ExN2VkMzRlMDRhMzM5MzMxYmIwNWFmZTg4YzQzIiwidXNlcl9pZCI6MTA2fQ.RTo8YMpmP2yUwPqyuGtlzM9T1w4D2oDhlJBKvPwPZQ0",
+    },
+  })
+    .then((response) => response.json())
+    .then((partidas) => {
+      console.log(JSON.stringify(partidas));
+      partidas.forEach((game) => {
+        console.log(`game.creator.username: ${game.creator.username}`);
+        console.log(`game.players: ${game.players}`);
+        console.log(`game.question_time: ${game.question_time}`);
+        console.log(`game.answer_time ${game.answer_time}`);
+        console.log(`game.rounds_number ${game.rounds_number}`);
+        console.log(`game.started: ${game.started}`);
+        console.log(`game.ended ${game.ended}`);
+        if (!game.started && !game.ended) {
+          // create parent div element
+          const parentDiv = document.createElement("div");
+          parentDiv.className =
+            "bg-white p-5 rounded-lg text-secondary shadow-lg mb-2";
 
-  // fetch("http://localhost:3000/partidas")
-  // .then((response) => response.json())
-  // .then((partidas) => {
-  //     crearPartidaBtn.addEventListener("click", () => {
-  //       crearPartida(partidas);
-  //     });
-  //   });
+          // create child grid element
+          const gridDiv = document.createElement("div");
+          gridDiv.className = "grid grid-cols-5 gap-4";
 
-  //Por ahora
-  for (let i = 0; i < 6; i++) {
-    // create parent div element
-    const parentDiv = document.createElement("div");
-    parentDiv.className =
-      "bg-white p-5 rounded-lg text-secondary shadow-lg mb-2";
+          // create child div elements for each grid column
+          const col1Div = document.createElement("div");
+          col1Div.className = "col-span-1 text-left";
+          const col2Div = document.createElement("div");
+          col2Div.className = "col-span-1 flex items-center justify-center";
+          const col3Div = document.createElement("div");
+          col3Div.className = "col-span-1 flex items-center justify-center";
+          const col4Div = document.createElement("div");
+          col4Div.className = "col-span-1 flex items-center justify-center";
+          const col5Div = document.createElement("div");
+          col5Div.className = "col-span-1 text-right";
 
-    // create child grid element
-    const gridDiv = document.createElement("div");
-    gridDiv.className = "grid grid-cols-3 gap-4";
+          // create child p element for the first column
+          const pElement = document.createElement("p");
+          pElement.className = "font-bold text-xl";
+          pElement.textContent = game.name;
 
-    // create child div elements for each grid column
-    const col1Div = document.createElement("div");
-    col1Div.className = "col-span-1 text-left";
-    const col2Div = document.createElement("div");
-    col2Div.className = "col-span-1 flex items-center justify-center";
-    const col3Div = document.createElement("div");
-    col3Div.className = "col-span-1 text-right";
+          // create child img element and span element for the second column
+          const imgElement = document.createElement("img");
+          imgElement.src =
+            "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png";
+          imgElement.className = "w-6 sm:w-10";
 
-    // create child p element for the first column
-    const pElement = document.createElement("p");
-    pElement.className = "font-bold text-xl";
-    pElement.textContent = "Partida " + (i + 1);
+          // create child element for the second column
+          const spanElementPlayerCount = document.createElement("span");
+          spanElementPlayerCount.className = "text-xl";
+          spanElementPlayerCount.textContent = game.player_count + "/12";
 
-    // create child img element and span element for the second column
-    const imgElement = document.createElement("img");
-    imgElement.src =
-      "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png";
-    imgElement.className = "w-6 sm:w-10";
+          // create child element for the third column
+          const spanElementQuestionTime = document.createElement("span");
+          spanElementQuestionTime.className = "text-xl";
+          spanElementQuestionTime.textContent = game.question_time;
 
-    const spanElement = document.createElement("span");
-    spanElement.className = "text-xl";
-    n = Math.floor(Math.random() * 12) + 1;
-    spanElement.textContent = " " + n + "/12";
+          // create child element for the fourth column
+          const spanElementAnswerTime = document.createElement("span");
+          spanElementAnswerTime.className = "text-xl";
+          spanElementAnswerTime.textContent = game.question_time;
 
-    // create child button element for the third column
-    const buttonElement = document.createElement("button");
-    buttonElement.id = "unirmeBtn";
-    buttonElement.className =
-      "bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg";
-    buttonElement.textContent = "Unirme";
+          // create child button element for the fifth column
+          const buttonElement = document.createElement("button");
+          buttonElement.onclick = function () {
+            location.href = "lobby.html";
+          };
+          buttonElement.id = "unirmeBtn-" + game.id;
+          buttonElement.className =
+            "bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg";
+          buttonElement.textContent = "Unirme";
 
-    // append child elements to their respective parent elements
-    col1Div.appendChild(pElement);
-    col2Div.appendChild(imgElement);
-    col2Div.appendChild(spanElement);
-    col3Div.appendChild(buttonElement);
-    gridDiv.appendChild(col1Div);
-    gridDiv.appendChild(col2Div);
-    gridDiv.appendChild(col3Div);
-    parentDiv.appendChild(gridDiv);
+          // append child elements to their respective parent elements
+          col1Div.appendChild(pElement);
+          col2Div.appendChild(imgElement);
+          col2Div.appendChild(spanElementPlayerCount);
+          col3Div.appendChild(spanElementQuestionTime);
+          col4Div.appendChild(spanElementAnswerTime);
+          col5Div.appendChild(buttonElement);
+          gridDiv.appendChild(col1Div);
+          gridDiv.appendChild(col2Div);
+          gridDiv.appendChild(col3Div);
+          gridDiv.appendChild(col4Div);
+          gridDiv.appendChild(col5Div);
+          parentDiv.appendChild(gridDiv);
 
-    // append parent div element to the container
-    const container = document.getElementById("contenedorPartidas");
-    container.appendChild(parentDiv);
-  }
+          // append parent div element to the container
+          const container = document.getElementById("contenedorPartidas");
+          container.appendChild(parentDiv);
+        }
+      });
+    });
 }
