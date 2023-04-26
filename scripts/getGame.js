7a483908773d56b789647d3f5988b94567c90065
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-04-25 20:42:42 -0400

    add content-type to getGame

--- scripts/getGame.js
+++ scripts/getGame.js
@@ -1,24 +1,24 @@
 import { customFetch } from "./fetch.js";
 
 export async function getGame(game_id) {
-    const games = {
-      url: "https://trivia-bck.herokuapp.com/api/games/",
-      args: {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("accessToken"),
-        },
+  const games = {
+    url: "https://trivia-bck.herokuapp.com/api/games/",
+    args: {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("accessToken"),
       },
-    };
-  
-    try {
-      const response = await customFetch(games.url, games.args);
-      const partidas = await response.json();
-      // get game with the id passed as parameter
-      const game = partidas.find((game) => game.id === Number(game_id));
-      console.log(game)
-      return game;
-    } catch (error) {
-      throw error;
-    }
+    },
+  };
+
+  try {
+    const response = await customFetch(games.url, games.args);
+    const partidas = await response.json();
+    // get game with the id passed as parameter
+    const game = partidas.find((game) => game.id === Number(game_id));
+    console.log(game);
+    return game;
+  } catch (error) {
+    throw error;
   }
-  
+}
