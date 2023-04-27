4146850abe2bf60ee3cf54ad1c6895c3219d69e1
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-04-27 06:19:34 -0400

    update websocket
    fix getting players from started game
    round_started
    round_question

--- scripts/websocket.js
+++ scripts/websocket.js
@@ -1,7 +1,12 @@
 import { getGame } from "./getGame.js";
+import { getStartedGame } from "./getStartedGame.js";
 import { addPlayersLobby } from "./addPlayersLobby.js";
+import { addPlayersInGame } from "./addPlayersInGame.js";
 import { refreshAccessToken } from "./fetch.js";
 import { getProfile } from "./getProfile.js";
+import { drawGameContainer } from "./drawGameContainer.js";
+import { drawQuestion } from "./drawQuestion.js";
+
 
 export let ws = null;
 
@@ -16,18 +21,21 @@ export function getWebSocket(game_id) {
     const token_access = localStorage.getItem("accessToken");
     const url = `wss://trivia-bck.herokuapp.com/ws/trivia/${game_id}/?token=${token_access}`;
     const ws = new WebSocket(url);
-    let game;
     ws.onopen = async function (event) {
       console.log("Conectado");
-      let game = await getGame(game_id);
-      // FIXME: Cant get players from started game
-      
-      console.log({gameOnOpen:game})
-      addPlayersLobby(game);
+      let game = await getStartedGame(game_id);
+      if (game.message === "El juego aun no ha comenzado.") {
+        game = await getGame(game_id);
+        addPlayersLobby(game);
+      }
     };
     ws.onmessage = async function (event) {
-      console.log(event);
+      console.log(event.data);
       const data = JSON.parse(event.data);
+      let game;
+      let user;
+      let nosy_id;
+      let question;
       if (data.type === "player_joined") {
         game = await getGame(game_id);
         addPlayersLobby(game);
@@ -39,6 +47,21 @@ export function getWebSocket(game_id) {
         location.href = "home.html";
       } else if (data.type === "game_started") {
         location.href = `partida.html?game_id=${game_id}`;
+      } else if (data.type === "round_started") {
+        console.log("Round started");
+        user = await getProfile();
+        nosy_id = data.nosy_id;
+        console.log({user})
+        console.log({nosy_id})
+        drawGameContainer(user.id, nosy_id);
+        game = await getStartedGame(game_id);
+        addPlayersInGame(game.players, nosy_id, user.id);
+      } else if (data.type === "round_question") {
+        user = await getProfile();
+        game = await getStartedGame(game_id);
+        nosy_id = game.round.nosy;
+        question = data.question;
+        drawQuestion(user.id, nosy_id, question);
       }
     };
     ws.onclose = function (event) {
