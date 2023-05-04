54998e7137619ed8548610525c7c359ab7b71ffd
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-05-04 12:28:55 -0400

    hide answer container when being pregunton

--- scripts/drawGameContainer.js
+++ scripts/drawGameContainer.js
@@ -1,12 +1,14 @@
 export function drawGameContainer(user_id, nosy_id) {
+    const answerContainer = document.getElementById("answerContainer");
     if (user_id === nosy_id) {
+        answerContainer.classList.add("hidden");
+        
         const preguntonPreguntaContainer = document.getElementById("preguntonPreguntaContainer");
         const preguntonRespuestaContainer = document.getElementById("preguntonRespuestaContainer");
 
         preguntonPreguntaContainer.classList.remove("hidden");
         preguntonRespuestaContainer.classList.remove("hidden");
     } else {
-        const answerContainer = document.getElementById("answerContainer");
         answerContainer.classList.remove("hidden");
 
         const textoPreguntaPlayer = document.getElementById("textoPreguntaPlayer");
