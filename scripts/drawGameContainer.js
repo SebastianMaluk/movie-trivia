c834d143c8668b5d9211887a397b915b9d84dbf2
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-05-08 07:17:52 -0400

    reset question and answer boxes for pregunton

--- scripts/drawGameContainer.js
+++ scripts/drawGameContainer.js
@@ -5,14 +5,28 @@ export function drawGameContainer(user_id, nosy_id) {
     const preguntonPreguntaContainer = document.getElementById(
       "preguntonPreguntaContainer"
     );
+    const abrirModalPreguntaBtn = document.getElementById(
+        "abrirModalPreguntaBtn"
+    );
+
     const preguntonRespuestaContainer = document.getElementById(
       "preguntonRespuestaContainer"
+    );
+    const abrirModalRespuestaBtn = document.getElementById(
+        "abrirModalRespuestaBtn"
     );
     const respuestasContainer = document.getElementById("incomingAnswers");
 
     preguntonPreguntaContainer.classList.remove("hidden");
+    abrirModalPreguntaBtn.classList.remove("hidden");
     preguntonRespuestaContainer.classList.remove("hidden");
+    abrirModalRespuestaBtn.classList.remove("hidden");
     respuestasContainer.classList.remove("hidden");
+
+    const textoPreguntaPregunton = document.getElementById(
+        "textoPreguntaPregunton"
+    );
+    textoPreguntaPregunton.textContent = "Pregunta:";
   } else {
     questionContainer.classList.remove("hidden");
     answerContainer.classList.remove("hidden");
