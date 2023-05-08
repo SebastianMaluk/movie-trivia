9f137f87d8f582c32121d673ebfe940b0506d7ff
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-05-07 22:36:14 -0400

    move scoreboard to html

--- scripts/addPlayersInGame.js
+++ scripts/addPlayersInGame.js
@@ -1,7 +1,6 @@
 export function addPlayersInGame(players, nosy_id, user_id) {
-  const scoreboard = document.getElementById("bordeParticipantes");
+  const scoreboard = document.getElementById("scoreboardContent");
   scoreboard.innerHTML = "";
-  drawScoreboardHeader();
   const jpg = "../imgs/question-mark.svg";
   players.forEach((player) => {
     let row_container = document.createElement("div");
@@ -68,58 +67,4 @@ export function addPlayersInGame(players, nosy_id, user_id) {
     col3.innerHTML = player.score;
     row.appendChild(col3);
   });
-}
-
-function drawScoreboardHeader() {
-  const div1 = document.createElement("div");
-  div1.id = "scoreboard_header";
-  div1.classList.add("my-2", "rounded", "shadow-md", "text-xs", "min-w-fit");
-
-  const div2 = document.createElement("div");
-  div2.classList.add(
-    "flex",
-    "items-start",
-    "bg-gray-200",
-    "px-2",
-    "py-2",
-    "min-w-fit"
-  );
-
-  const div3 = document.createElement("div");
-  div3.classList.add("w-4/12", "text-center", "text-red-700");
-  div3.textContent = "Jugadores";
-
-  const div4 = document.createElement("div");
-  div4.classList.add(
-    "w-5/12",
-    "flex",
-    "justify-end",
-    "items-center",
-    "text-red-700"
-  );
-
-  const p1 = document.createElement("p");
-  p1.classList.add("w-5/12", "px-2", "text-center");
-  p1.textContent = "Errores";
-
-  const p2 = document.createElement("p");
-  p2.classList.add("w-7/12", "px-2", "text-center");
-  p2.textContent = "Descalificado";
-
-  const div5 = document.createElement("div");
-  div5.classList.add("w-3/12", "text-red-700", "text-right");
-  div5.textContent = "Puntaje";
-
-  div4.appendChild(p1);
-  div4.appendChild(p2);
-
-  div2.appendChild(div3);
-  div2.appendChild(div4);
-  div2.appendChild(div5);
-
-  div1.appendChild(div2);
-
-  const scoreboard = document.getElementById("bordeParticipantes");
-
-  scoreboard.appendChild(div1);
 }
