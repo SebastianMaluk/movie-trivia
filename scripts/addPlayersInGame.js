311233128f0ecb1f7b6f61a88820054a8d15d7ab
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-05-08 07:12:05 -0400

    prettify scoreboard

--- scripts/addPlayersInGame.js
+++ scripts/addPlayersInGame.js
@@ -2,6 +2,10 @@ export function addPlayersInGame(players, nosy_id, user_id) {
   const scoreboard = document.getElementById("scoreboardContent");
   scoreboard.innerHTML = "";
   const jpg = "../imgs/question-mark.svg";
+//   sort players by score
+  players.sort((a, b) => {
+    return b.score - a.score;
+  });
   players.forEach((player) => {
     let row_container = document.createElement("div");
     row_container.className =
@@ -42,24 +46,23 @@ export function addPlayersInGame(players, nosy_id, user_id) {
 
     // add child to row
     let col2 = document.createElement("div");
-    col2.className = "w-5/12 flex justify-end items-center";
+    col2.className = "w-5/12 flex justify-center inline items-center";
     row.appendChild(col2);
 
     // add child to col2
-    let col2p1 = document.createElement("p");
-    col2p1.className = "w-5/12 px-1 text-center";
-    col2p1.innerHTML = player.faults;
+    let col2p1 = document.createElement("span");
+    col2p1.className = "flex";
+    for (let i = 0; i < player.faults; i++) {
+      let img = document.createElement("img");
+      img.className = "w-6 self-center";
+      img.src = "../imgs/fault.svg";
+      col2p1.appendChild(img);
+    }
     col2.appendChild(col2p1);
 
-    // add child to col2
-    let col2p2 = document.createElement("p");
-    col2p2.className = "w-7/12 px-1 text-center";
-    if (player.faults >= 3) {
-      col2p2.innerHTML = "SI";
-    } else {
-      col2p2.innerHTML = "NO";
+    if (player.faults === 3) {
+        row.classList.add("bg-red-300");
     }
-    col2.appendChild(col2p2);
 
     // add child to row
     let col3 = document.createElement("p");
