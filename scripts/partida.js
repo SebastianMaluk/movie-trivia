aa66f2eab05e5c01d85f44add082335ececae121
javiersoto99] <jisoto2@miuandes.cl>
2023-05-08 01:33:42 -0400

    end timer conditions

--- scripts/partida.js
+++ scripts/partida.js
@@ -50,52 +50,55 @@ window.addEventListener("load", async function () {
   const ws = new CustomWebSocket();
   await ws.setUp();
 
-abrirModalPreguntaBtn.onclick = function () {
-  preguntaModal.classList.remove("hidden");
-  blurFondo.classList.remove("hidden");
-};
+  abrirModalPreguntaBtn.onclick = function () {
+    preguntaModal.classList.remove("hidden");
+    blurFondo.classList.remove("hidden");
+  };
 
-cerrarModalPreguntaBtn.onclick = function () {
-  preguntaModal.classList.add("hidden");
-  blurFondo.classList.add("hidden");
-};
+  cerrarModalPreguntaBtn.onclick = function () {
+    preguntaModal.classList.add("hidden");
+    blurFondo.classList.add("hidden");
+  };
 
-enviarPreguntaBtn.onclick = function (event) {
-  event.preventDefault();
-  var pregunta = preguntaPreguntonInput.value;
-  textoPreguntaPregunton.innerText = pregunta;
-  abrirModalPreguntaBtn.classList.add("hidden");
-  preguntaModal.classList.add("hidden");
-  blurFondo.classList.add("hidden");
-  ws.sendQuestion(pregunta);
-};
+  enviarPreguntaBtn.onclick = function (event) {
+    const questionTimeContainer = document.getElementById("tiempoPreguntar");
+    event.preventDefault();
+    var pregunta = preguntaPreguntonInput.value;
+    textoPreguntaPregunton.innerText = pregunta;
+    abrirModalPreguntaBtn.classList.add("hidden");
+    preguntaModal.classList.add("hidden");
+    blurFondo.classList.add("hidden");
+    ws.sendQuestion(pregunta);
+    questionTimeContainer.innerText = 0;
+  };
 
-abrirModalRespuestaBtn.onclick = function () {
-  respuestaModal.classList.remove("hidden");
-  blurFondo.classList.remove("hidden");
-};
+  abrirModalRespuestaBtn.onclick = function () {
+    respuestaModal.classList.remove("hidden");
+    blurFondo.classList.remove("hidden");
+  };
 
-cerrarModalRespuestaBtn.onclick = function () {
-  respuestaModal.classList.add("hidden");
-  blurFondo.classList.add("hidden");
-};
+  cerrarModalRespuestaBtn.onclick = function () {
+    respuestaModal.classList.add("hidden");
+    blurFondo.classList.add("hidden");
+  };
 
-enviarRespuestaPreguntonBtn.onclick = function (event) {
-  event.preventDefault();
-  var respuesta = respuestaCorrectaInput.value;
-  textoRespuesta.innerText = respuesta;
-  ws.sendAnswer(respuesta);
-  abrirModalRespuestaBtn.classList.add("hidden");
-  respuestaModal.classList.add("hidden");
-  blurFondo.classList.add("hidden");
-};
+  enviarRespuestaPreguntonBtn.onclick = function (event) {
+    event.preventDefault();
+    var respuesta = respuestaCorrectaInput.value;
+    textoRespuesta.innerText = respuesta;
+    ws.sendAnswer(respuesta);
+    abrirModalRespuestaBtn.classList.add("hidden");
+    respuestaModal.classList.add("hidden");
+    blurFondo.classList.add("hidden");
+  };
 
-enviarRespuestaPlayerBtn.onclick = function (event) {
-  let respuesta = respuestaInput.value;
-  ws.sendAnswer(respuesta);
-  const answerContainer = document.getElementById("answerContainer");
-  answerContainer.classList.add("hidden");
-  respuestaInput.value = "";
-};
-
+  enviarRespuestaPlayerBtn.onclick = function (event) {
+    const answerTimeContainer = document.getElementById("tiempoResponder");
+    let respuesta = respuestaInput.value;
+    ws.sendAnswer(respuesta);
+    const answerContainer = document.getElementById("answerContainer");
+    answerContainer.classList.add("hidden");
+    respuestaInput.value = "";
+    answerTimeContainer.innerText = 0;
+  };
 });
